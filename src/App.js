0a2434329b9628c6c9cbc429d203051b5da97b1e
AmirHossein Ebrahimi <amirhosseinebrahimi77@gmail.com>
2019-02-04 00:11:27 +0330

    Add basic start point App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+// Component
+import SignUp from './frontend/components/auth/signUp'
+// import SignIn from './frontend/components/auth/signIn'
+
+/* App */
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SignUp />
+      </div>
+    )
+  }
+}
+export default App
