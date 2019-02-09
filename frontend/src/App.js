486da9874cb6970f008e9aa473af8041d4400cae
AmirHossein Ebrahimi <amirhosseinebrahimi77@gmail.com>
2019-02-09 11:25:29 +0330

    basic config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 // Component
-// import SignUp from './frontend/components/auth/signUp'
-import SignIn from './frontend/components/auth/signIn'
+// import SignUp from './components/auth/signUp'
+// import SignIn from './components/auth/signIn'
+import Pack from './components/form/pack'
 
 /* App */
 class App extends Component {
   render() {
     return (
       <div>
-        <SignIn />
+        <Pack from={1} />
+        <Pack from={11} />
       </div>
     )
   }
