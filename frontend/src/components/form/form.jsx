1c13d1d7803ba6aeff759fc63ac6776e028e5fa2
AmirHossein Ebrahimi <amirhosseinebrahimi77@gmail.com>
2019-02-09 11:25:09 +0330

    add form component

---
+++ frontend/src/components/form/form.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// component
+import Pack from './pack'
+
+export default function Form() {
+  return (
+    <div>
+      <Pack from={1} count={10} />
+    </div>
+  )
+}
