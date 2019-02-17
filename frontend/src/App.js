8b5429bdfdf9e580eb5515143d35a430625da07f
AmirHossein Ebrahimi <amirhosseinebrahimi77@gmail.com>
2019-02-17 18:49:31 +0330

    remove changed file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react'
 // Component
 // import SignUp from './components/auth/signUp'
 // import SignIn from './components/auth/signIn'
-import Pack from './components/form/pack'
-import Submit from './components/form/submit'
+// import Pack from './components/form/pack'
+// import Form from './helper/components/form'
+import Forms from './components/forms/list'
+import Submit from './helper/components/animatedButton'
 // style
 import './App.scss'
 // import { getForm } from './helper/requestHandler'
@@ -13,8 +15,50 @@ class App extends Component {
   render() {
     return (
       <div style={{ display: 'flex' }}>
-        <Pack from={1} count={240} />
-        <Submit />
+        {/* <Submit text="submit" /> */}
+        <Forms
+          forms={[
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            {
+              formName:
+                'Azmon Reyaziklhsadglkhadklshgklhaklsdghklhaklgklhsgkl 2',
+              formId: '2',
+            },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            {
+              formName: 'Azmon klahsgklhasklhglkahgklhaklshgklhgasklReyazi 2',
+              formId: '2',
+            },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+            { formName: 'Azmon Reyazi 2', formId: '2' },
+          ]}
+        />
       </div>
     )
   }
