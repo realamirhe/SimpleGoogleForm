c07f56bd96317982fb4678ec6ae83d61870f7d21
AmirHossein Ebrahimi <amirhosseinebrahimi77@gmail.com>
2019-02-17 20:29:08 +0330

    refactor(frontend): Reset App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,85 +1,8 @@
-import React, { Component } from 'react'
+import React from 'react'
 // Component
-// import SignUp from './components/auth/signUp'
-// import SignIn from './components/auth/signIn'
-// import Pack from './components/form/pack'
-// import Form from './helper/components/form'
-import Forms from './components/forms/list'
 // style
 import './App.scss'
-// import { getForm } from './helper/requestHandler'
 
 /* App */
-class App extends Component {
-  render() {
-    return (
-      <div style={{ display: 'flex' }}>
-        {/* <Submit text="submit" /> */}
-        <Forms
-          forms={[
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            {
-              formName:
-                'Azmon Reyaziklhsadglkhadklshgklhaklsdghklhaklgklhsgkl 2',
-              formId: '2',
-            },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            {
-              formName: 'Azmon klahsgklhasklhglkahgklhaklshgklhgasklReyazi 2',
-              formId: '2',
-            },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            {
-              formName: 'Azmon klahsgklhasklhglkahgklhaklshgklhgasklReyazi 2',
-              formId: '2',
-            },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            {
-              formName: 'Azmon klahsgklhasklhglkahgklhaklshgklhgasklReyazi 2',
-              formId: '2',
-            },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-            { formName: 'Azmon Reyazi 2', formId: '2' },
-          ]}
-        />
-      </div>
-    )
-  }
-}
+const App = props => <div style={{ display: 'flex' }} />
 export default App
