7cc342f8d9e97f72c004733dbd5e5d9bdaec4a95
AmirHossein Ebrahimi <amirhosseinebrahimi77@gmail.com>
2019-02-17 19:16:31 +0330

    add icon to item and list

--- frontend/src/components/forms/list/list.jsx
+++ frontend/src/components/forms/list/list.jsx
@@ -2,15 +2,26 @@ import React from 'react'
 import PropTypes from 'prop-types'
 // third-party-packages
 import { map } from 'ramda'
+// assets
+import NoteAdd from '@material-ui/icons/NoteAdd'
 // component
 import MiniFormItem from '../item'
+import Icon from '../../../helper/components/Icon'
+
 // style
 import './style.scss'
 
+// TODO: onClick Add Icon
 /* Mini Form List  */
 const MiniFormList = ({ forms }) => {
   return (
     <div className="mini-form__list">
+      <Icon
+        color="primary"
+        icon={<NoteAdd style={{ width: 30, height: 30 }} />}
+        ariaLabel="note-add"
+        size="large"
+      />
       {map(
         ({ formName, formId }) => (
           <MiniFormItem formName={formName} formId={formId} />
