40ff92c3caae96ed263d1b5aa765ceef4045d440
AmirHossein Ebrahimi <amirhosseinebrahimi77@gmail.com>
2019-02-19 21:45:18 +0330

    feat(front): Add Upload Button

--- frontend/src/helper/components/buttons/index.js
+++ frontend/src/helper/components/buttons/index.js
@@ -1,2 +1,3 @@
 export { default as ProgressButton } from './progressButton'
 export { default as Button } from './button'
+export { default as UploadButton } from './uploadButton'
---
+++ frontend/src/helper/components/buttons/uploadButton.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+// third-party-packages
+import Button from '@material-ui/core/Button'
+// style
+import { withStyles } from '@material-ui/core/styles'
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+  input: {
+    display: 'none',
+  },
+})
+
+function UploadButton({ onChange, classes }) {
+  return (
+    <div>
+      <input
+        accept="application/pdf"
+        className={classes.input}
+        id="contained-button-file"
+        multiple
+        type="file"
+        onChange={onChange}
+      />
+      <label htmlFor="contained-button-file">
+        <Button
+          color="secondary"
+          variant="contained"
+          component="span"
+          className={classes.button}
+        >
+          Upload
+        </Button>
+      </label>
+    </div>
+  )
+}
+
+UploadButton.propTypes = {
+  onChange: PropTypes.func.isRequired,
+}
+
+export default withStyles(styles)(UploadButton)
