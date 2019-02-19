e7b10b11b13fa5823efd7db15df59b7d8b618910
AmirHossein Ebrahimi <amirhosseinebrahimi77@gmail.com>
2019-02-19 18:11:34 +0330

    test(front): App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,57 @@
 import React from 'react'
 // Component
-import FormMaker from './components/users/admin/formMaker'
+// import FormMaker from './components/users/admin/formInfo'
+import Form from './helper/components/form'
 // style
 import './App.scss'
 
 /* App */
 const App = props => (
-  <FormMaker />
+  <div>
+    <Form
+      disableSound
+      admin
+      initialQuestionCount={35}
+      initialQuestions={[
+        2,
+        2,
+        2,
+        2,
+        null,
+        4,
+        4,
+        3,
+        null,
+        1,
+        2,
+        2,
+        2,
+        2,
+        3,
+        2,
+        2,
+        2,
+        2,
+        null,
+        4,
+        4,
+        3,
+        null,
+        1,
+        2,
+        2,
+        2,
+        2,
+        3,
+        null,
+        null,
+        null,
+        null,
+        null,
+      ]}
+      initialFormName="cost(nnt)"
+    />
+  </div>
   // <Router>
   // <Route authenticateURL component={signIn}/>
   // </Router>
